d2c6d908f00045ab559e4d9d8d74999192bda473
iisento <1215601.buckup@gmail.com>
2017-08-14 15:19:25 +0900

    add test for knock01

---
+++ ch01/knock01_test.py
@@ -0,0 +1,10 @@
+import unittest
+import knock01
+
+class TestKnock01(unittest.TestCase):
+    def test_reverse_string(self):
+        self.assertEqual(knock01.split_patato("パタトクカシー", 1), "パトカー")
+        self.assertEqual(knock01.split_patato("パタトクカシー", 1), "タクシー")
+
+if __name__ == "__main__":
+    unittest.main()
