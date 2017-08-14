9d412d9caaa6bf70d02463bcde5acd6f2ae89066
iisento <1215601.buckup@gmail.com>
2017-08-14 15:43:07 +0900

    fix coverage

--- ch01/knock01_test.py
+++ ch01/knock01_test.py
@@ -2,9 +2,6 @@ import unittest
 import knock01
 
 class TestKnock01(unittest.TestCase):
-    def test_reverse_string(self):
+    def test_split_patato(self):
         self.assertEqual(knock01.split_patato("パタトクカシーー", 0), "パトカー")
         self.assertEqual(knock01.split_patato("パタトクカシーー", 1), "タクシー")
-
-if __name__ == "__main__":
-    unittest.main()
