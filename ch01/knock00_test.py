b2e7679b0763a44909dec443eca52d85c2a78a34
iisento <1215601.buckup@gmail.com>
2017-08-14 16:05:03 +0900

    apply pep8

--- ch01/knock00_test.py
+++ ch01/knock00_test.py
@@ -1,6 +1,7 @@
 import unittest
 import knock00
 
+
 class TestKnock00(unittest.TestCase):
     def test_reverse_string(self):
         self.assertEqual(knock00.reverse_string("あいうえお"), "おえういあ")
--- ch01/knock01_test.py
+++ ch01/knock01_test.py
@@ -1,6 +1,7 @@
 import unittest
 import knock01
 
+
 class TestKnock01(unittest.TestCase):
     def test_split_patato(self):
         self.assertEqual(knock01.split_patato("パタトクカシーー", 0), "パトカー")
