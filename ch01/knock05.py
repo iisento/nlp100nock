997b5d966f3895465d00b0031c25bbfa33cdf218
iisento <1215601.buckup@gmail.com>
2017-08-24 01:58:08 +0900

    solve knock05

--- ch01/knock05.py
+++ ch01/knock05.py
@@ -1,7 +1,18 @@
 class NGram:
 
-    def __init__(self, sentense):
-        self._sentense = sentense
+    def __init__(self, sentence):
+        self._sentence = sentence
 
     def word(self, dim):
-        pass
+        i = dim - 1
+        s_list = self._sentence.split()
+        s_list1 = s_list[:-i]
+        s_list2 = s_list[i:]
+        return [list(s) for s in zip(s_list1, s_list2)]
+
+    def char(self, dim):
+        i = dim - 1
+        s = self._sentence.replace(' ', '')
+        s1 = s[:-i]
+        s2 = s[i:]
+        return [list(s) for s in zip(s1, s2)]
