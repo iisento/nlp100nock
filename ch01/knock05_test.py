7b05695b65de765dcec15aaa3ec1f440d5a9bab1
iisento <1215601.buckup@gmail.com>
2017-08-24 01:28:34 +0900

    change tupple to list

--- ch01/knock05_test.py
+++ ch01/knock05_test.py
@@ -8,9 +8,9 @@ class TestKnock05(unittest.TestCase):
         n_gram = knock05.NGram(sample_sentence)
         word_bi_gram = n_gram.word(2)
         char_bi_gram = n_gram.char(2)
-        word_bi_gram_answer = [('I', 'am'), ('am', 'an'), ('an', 'NLPer')]
-        char_bi_gram_answer = [('I', 'a'), ('a', 'm'), ('m', 'a'),
-                               ('a', 'n'), ('n', 'N'), ('N', 'L'),
-                               ('L', 'P'), ('P', 'e'), ('e', 'r')]
+        word_bi_gram_answer = [['I', 'am'], ['am', 'an'], ['an', 'NLPer']]
+        char_bi_gram_answer = [['I', 'a'], ['a', 'm'], ['m', 'a'],
+                               ['a', 'n'], ['n', 'N'], ['N', 'L'],
+                               ['L', 'P'], ['P', 'e'], ['e', 'r']]
         self.assertEqual(word_bi_gram, word_bi_gram_answer)
         self.assertEqual(char_bi_gram, char_bi_gram_answer)
