8e26f5bf9b78f9b03d7f439c5060b4035b5283f5
iisento <1215601.buckup@gmail.com>
2017-08-25 00:09:36 +0900

    change because replace() is faster than re.sub()

--- ch01/knock03.py
+++ ch01/knock03.py
@@ -4,7 +4,7 @@ import re
 
 def generate_pi():
     s1 = 'Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics.'
-    s2 = re.sub(',|\.', '', s1).split()
+    s2 = s1.replace(',', '').replace('.', '').split()
     s3 = []
     for i in s2:
         s3.append(len(i))
