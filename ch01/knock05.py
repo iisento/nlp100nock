ef57262e47e4ddaad479629a027028c7b3497eca
iisento <1215601.buckup@gmail.com>
2017-08-27 04:05:28 +0900

    add set

--- ch01/knock05.py
+++ ch01/knock05.py
@@ -3,18 +3,24 @@ class NGram:
     def __init__(self, sentence):
         self._sentence = sentence
 
-    def word(self, dim):
+    def word(self, dim=2, type='list'):
         s_list = self._sentence.split()
         s_list_len = len(s_list)
         s_lists = []
         for i in range(dim):
             s_lists.append(s_list[i: s_list_len - (1 - i)])
-        return [list(s) for s in zip(*s_lists)]
+        if type == 'list':
+            return [list(s) for s in zip(*s_lists)]
+        elif type == 'set':
+            return {frozenset(s) for s in zip(*s_lists)}
 
-    def char(self, dim):
+    def char(self, dim=2, type='list'):
         s = self._sentence.replace(' ', '')
         s_len = len(s)
         s_list = []
         for i in range(dim):
             s_list.append(s[i: s_len - (i - 1)])
-        return [list(s) for s in zip(*s_list)]
+        if type == 'list':
+            return [list(s) for s in zip(*s_list)]
+        elif type == 'set':
+            return {frozenset(s) for s in zip(*s_list)}
