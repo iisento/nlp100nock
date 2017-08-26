9eb6e151b551442b3ebb7b9e3fb391260c623d4b
iisento <1215601.buckup@gmail.com>
2017-08-27 04:05:52 +0900

    add test for knock06

---
+++ ch01/knock06_test.py
@@ -0,0 +1,11 @@
+import unittest
+import knock05
+import knock06
+
+
+class TestKnock06:
+    x = 'paraparaparadise'
+    y = 'paragraph'
+    se = ['s', 'e']
+    X = knock05.NGram(s1)
+    Y = knock05.NGram(s2)
