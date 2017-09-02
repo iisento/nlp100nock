27853ea2de429b8f2e6cec8a30fd82f260c5cc41
iisento <1215601.buckup@gmail.com>
2017-09-02 21:08:37 +0900

    add script thats downloads hightemp.txt

---
+++ ch02/download_hightemp.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+echo "========= downloading hightemp.txt START ========"
+curl -o hightemp.txt http://www.cl.ecei.tohoku.ac.jp/nlp100/data/hightemp.txt
+echo "========= downloading hightemp.txt FINISH ========"
